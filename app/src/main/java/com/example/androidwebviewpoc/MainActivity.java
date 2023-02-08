243fcf196ed858424a6c962bbc91d9c1777d56c0
Kingdom Orjiewuru <kingdom.orjiewuru@gmail.com>
2023-02-08 12:27:20 +0400

    feat: load link sdk using hosted html with params

--- app/src/main/java/com/example/androidwebviewpoc/MainActivity.java
+++ app/src/main/java/com/example/androidwebviewpoc/MainActivity.java
@@ -16,6 +16,21 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
 
     private WebView leanWebview;
+    public String LEAN_SDK_BASE_URL = "https://lean-loader-test.s3.amazonaws.com/lean-sdk.html";
+
+    String initializationUrl = LEAN_SDK_BASE_URL +
+            "?env=ae03" + // Optional [defaults to prod]
+            "&method=connect" +
+            "&app_token=9fb9e934-9efb-4e7e-a508-de67c0839be0" +
+            "&customer_id=03bfa403-33b6-4ec5-96eb-28e822d1cdc1" +
+            "&permissions=balance" +
+            "&permissions=identity" +
+            "&permissions=accounts" +
+            "&permissions=payments" +
+            "&permissions=transactions" +
+            "&sandbox=true";
+            // "&callback='response => console.log(\"callback received:\", response)'"
+            // @TODO: Implement redirect in place of callbacks
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -25,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         leanWebview = (WebView) findViewById(R.id.webview);
         leanWebview.setWebViewClient(new WebViewClient());
-        leanWebview.loadUrl("https://google.com");
+        leanWebview.loadUrl(initializationUrl);
 
         WebSettings webSettings = leanWebview.getSettings();
         webSettings.setJavaScriptEnabled(true);
